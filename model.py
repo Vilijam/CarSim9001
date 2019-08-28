efef6a64e4fec2cf73dc35660651c6448be0bf39
Vilijam <37106752+Vilijam@users.noreply.github.com>
2019-08-28 14:36:36 +0200

    Tank is done Wheel is done Gearbox er begyndt Resten ain't done

--- model.py
+++ model.py
@@ -4,13 +4,48 @@ class Car(object):
     pass
 
 class Wheel(object):
-    pass
+
+    def __init__(self):
+        self.orientation = randint(1,360)
+
+    def rotate(self, revolutions):
+        self.orientation = (self.orientation + (revolutions * 360)) % 360
 
 class Engine(object):
     pass
 
 class Gearbox(object):
-    pass
+
+    def __init__(self):
+        self.wheels = {}
+        self.currentGear = 0
+        self.clutchEngaged = False
+        self.gears [0, 0.8, 1, 1.4, 2.2, 3.8]
+
+        listOWheels = ["frontLeft", "frontRight", "rearLeft", "rearRight"]
+        for e in listOWheels:
+            self.wheels[e] = Wheel()
+
+    def shiftUp(self):
+        pass
+
+    def shiftDown(self):
+        pass
+
+    def rotate(self, revolutions):
+        pass
 
 class Tank(object):
-    pass
+
+    def __init__(self):
+        self.capacity = 100
+        self.contents = 100
+
+    def refuel(self):
+        self.contents = self.capacity
+
+    def remove(self, amount):
+        if(self.contents - amount < 0):
+            self.contents = 0
+        else:
+            self.contents = self.contents - amount
